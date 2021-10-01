905cc7a6d91fb5244a1b1f916a58fb64516bca39
SalouaK <saloua.khettab@gmail.com>
2021-10-01 16:28:02 +0200

    update redux form into react final form

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,11 +1,10 @@
 import { combineReducers } from 'redux';
-import { reducer as formReducer } from 'redux-form';
+
 import authReducer from './authReducer';
 import streamsReducer from './streamsReducer';
 
 export default combineReducers({
    auth: authReducer,
-   // reducer of redux-form, that's all we have to do
-   form: formReducer,
+   // reducer of react-final-form, that's all we have to do
    streams: streamsReducer
 });
